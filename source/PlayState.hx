dd1f257e05cab3eb349fa45292f052dc80c348de
Andre Dantas <andre.dantas.lima@gmail.com>
2017-12-25 20:57:13 -0500

    tutorial

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,14 +5,15 @@ import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
-	override public function create():Void
-	{
+	private var _player:Player;
+	
+	override public function create():Void {
+		_player = new Player(20, 20);
+    add(_player);
 		super.create();
-		add(new FlxText(100, 50, 100, "Hello, World!", 24));
 	}
 
-	override public function update(elapsed:Float):Void
-	{
+	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 	}
 }
