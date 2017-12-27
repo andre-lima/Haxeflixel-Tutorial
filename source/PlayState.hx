a06c7eb1acdc880fe424af3e57bf360a7a2355ab
Andre Dantas <andre.dantas.lima@gmail.com>
2017-12-27 17:12:17 -0500

    Tilemaps

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,10 +6,18 @@ import flixel.text.FlxText;
 class PlayState extends FlxState
 {
 	private var _player:Player;
+	private var _map:TiledLevel;
 	
 	override public function create():Void {
 		_player = new Player(20, 20);
     add(_player);
+
+		// Load the level's tilemaps
+		_map = new TiledLevel("assets/data/maps/map.tmx", this);
+		// Add tilemaps
+		
+	  //add(_map.background);
+
 		super.create();
 	}
 
